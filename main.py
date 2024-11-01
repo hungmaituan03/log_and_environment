a745ade43560cc1bbbd2b4f820d0413eb425d67b
hungmaituan03 <hungmaituan03@gmail.com>
2024-11-01 15:17:00 -0400

    Add GitHub Actions, environment variables, and logging setup

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 # main.py
+import logging
+import logging.config
+from dotenv import load_dotenv
+import os
 from app import App    
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access environment variables
+environment = os.getenv('ENVIRONMENT')
+debug = os.getenv('DEBUG')
+
+print(f"Environment: {environment}")
+print(f"Debug: {debug}")
+
+# Load logging configuration
+logging.config.fileConfig('logging.conf')
+
+# Create a logger
+logger = logging.getLogger(__name__)
+
+# Log a test message
+logger.info('This is a debug message')
 
 # You must put this in your main.py because this forces the program to start when you run it from the command line.
 if __name__ == "__main__":
+    logger.info("Starting application...")
     app = App().start()  # Instantiate an instance of App
+    logger.info("Application started.")
+    
